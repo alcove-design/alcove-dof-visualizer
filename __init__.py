e83f74843186d6b5231e0455bcad2abcf6c0431d
Mehdi El Fadil <m.ef@mango-is.com>
2025-08-08 15:38:01 +0200

    bump version number #3

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
 	"name": "Alcove DoF Visualizer",
 	"author": "Alcove design - Mehdi El Fadil",
-	"version": (0, 1, 0),
+	"version": (0, 1, 3),
 	"blender": (4, 2, 0),
 	"location": "View3D > Overlays Popover",
 	"description": "Adds a real-time depth of field overlay and text info",
